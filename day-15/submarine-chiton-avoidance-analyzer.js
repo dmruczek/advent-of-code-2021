fd107645e35a85cb235652256391d88dc6db404b
Dave <dmruczek@gmail.com>
2021-12-18 21:25:10 -0500

    working on day 15 part 2

--- day-15/submarine-chiton-avoidance-analyzer.js
+++ day-15/submarine-chiton-avoidance-analyzer.js
@@ -4,14 +4,18 @@ module.exports = class SubmarineChitonAvoidanceAnalyzer {
 
     constructor() {}
 
-    loadInput(filename) {
+    loadInput(filename, enhanced) {
         const path = require('path');
         const fs = require('fs');
         const os = require('os');
 
         const data = fs.readFileSync(path.resolve(__dirname, filename), 'utf-8');
         const lines = data.split(os.EOL);
-        this.processInputData(lines);
+        if (enhanced) {
+            this.processInputDataEnhanced(lines);
+        } else {
+            this.processInputData(lines);
+        }
     }
 
     processInputData(lines) {
@@ -31,9 +35,84 @@ module.exports = class SubmarineChitonAvoidanceAnalyzer {
         this.chitonRiskMatrix[0][0] = 0;
     }
 
+    processInputDataEnhanced(lines) {
+        let chitonRiskMatrix = Array(lines.length * 5);
+        let aggregateRiskMatrix = Array(lines.length * 5);
+
+        for (let i = 0; i < chitonRiskMatrix.length; i++) {
+            chitonRiskMatrix[i] = Array(lines[0].length * 5);
+            aggregateRiskMatrix[i] = Array(lines[0].length * 5);
+        }
+
+        for (let y = 0; y < lines.length; y++) {
+            for (let x = 0; x < lines[y].length; x++) {
+                const thisNum = parseInt(lines[y].charAt(x), 10);
+
+                // for (let yO = 0; yO < 5; yO ++) {
+                //     for (let xO = 0; xO < 5; xO ++) {
+                //         chitonRiskMatrix[y][x] = this.adjustNumber(thisNum + 1); thisNum;
+
+                //     }
+                // }
+
+
+
+                chitonRiskMatrix[y][x] = thisNum;
+
+                chitonRiskMatrix[y + 1*(lines.length)][x] = this.adjustNumber(thisNum + 1);
+                chitonRiskMatrix[y + 2*(lines.length)][x] = this.adjustNumber(thisNum + 2);
+                chitonRiskMatrix[y + 3*(lines.length)][x] = this.adjustNumber(thisNum + 3);
+                chitonRiskMatrix[y + 4*(lines.length)][x] = this.adjustNumber(thisNum + 4);
+
+                chitonRiskMatrix[y + 0*(lines.length)][x + lines[0].length] = this.adjustNumber(thisNum + 1);
+                chitonRiskMatrix[y + 1*(lines.length)][x + lines[0].length] = this.adjustNumber(thisNum + 2);
+                chitonRiskMatrix[y + 2*(lines.length)][x + lines[0].length] = this.adjustNumber(thisNum + 3);
+                chitonRiskMatrix[y + 3*(lines.length)][x + lines[0].length] = this.adjustNumber(thisNum + 4);
+                chitonRiskMatrix[y + 4*(lines.length)][x + lines[0].length] = this.adjustNumber(thisNum + 5);
+
+                chitonRiskMatrix[y + 0*(lines.length)][x + 2*(lines[0].length)] = this.adjustNumber(thisNum + 2);
+                chitonRiskMatrix[y + 1*(lines.length)][x + 2*(lines[0].length)] = this.adjustNumber(thisNum + 3);
+                chitonRiskMatrix[y + 2*(lines.length)][x + 2*(lines[0].length)] = this.adjustNumber(thisNum + 4);
+                chitonRiskMatrix[y + 3*(lines.length)][x + 2*(lines[0].length)] = this.adjustNumber(thisNum + 5);
+                chitonRiskMatrix[y + 4*(lines.length)][x + 2*(lines[0].length)] = this.adjustNumber(thisNum + 6);
+
+                chitonRiskMatrix[y + 0*(lines.length)][x + 3*(lines[0].length)] = this.adjustNumber(thisNum + 3);
+                chitonRiskMatrix[y + 1*(lines.length)][x + 3*(lines[0].length)] = this.adjustNumber(thisNum + 4);
+                chitonRiskMatrix[y + 2*(lines.length)][x + 3*(lines[0].length)] = this.adjustNumber(thisNum + 5);
+                chitonRiskMatrix[y + 3*(lines.length)][x + 3*(lines[0].length)] = this.adjustNumber(thisNum + 6);
+                chitonRiskMatrix[y + 4*(lines.length)][x + 3*(lines[0].length)] = this.adjustNumber(thisNum + 7);
+
+                chitonRiskMatrix[y + 0*(lines.length)][x + 4*(lines[0].length)] = this.adjustNumber(thisNum + 4);
+                chitonRiskMatrix[y + 1*(lines.length)][x + 4*(lines[0].length)] = this.adjustNumber(thisNum + 5);
+                chitonRiskMatrix[y + 2*(lines.length)][x + 4*(lines[0].length)] = this.adjustNumber(thisNum + 6);
+                chitonRiskMatrix[y + 3*(lines.length)][x + 4*(lines[0].length)] = this.adjustNumber(thisNum + 7);
+                chitonRiskMatrix[y + 4*(lines.length)][x + 4*(lines[0].length)] = this.adjustNumber(thisNum + 8);
+
+            }
+        }
+        this.chitonRiskMatrix = chitonRiskMatrix;
+        this.aggregateRiskMatrix = aggregateRiskMatrix;
+        // Setting first position to risk 0, since you start here.
+        this.chitonRiskMatrix[0][0] = 0;
+    }
+
+    adjustNumber(num) {
+        if (num > 9) {
+            num -= 9;
+        }
+        return num;
+    }
+
+
+
     doRiskCalculation() {
         this.calculateAggregateRiskOfPosition(0,0);
-        this.secondAggregateRiskPass();
+        let shouldDoAdditoinalAggregateRiskPass = true;
+        let additionalPassNumber = 1;
+        while (shouldDoAdditoinalAggregateRiskPass) {
+            shouldDoAdditoinalAggregateRiskPass = this.doAdditionalAggregateRiskPass();
+            additionalPassNumber++;
+        }
         return this.aggregateRiskMatrix[0][0];
     }
 
@@ -69,8 +148,9 @@ module.exports = class SubmarineChitonAvoidanceAnalyzer {
 
 
 
-    secondAggregateRiskPass() {
+    doAdditionalAggregateRiskPass() {
 
+        let somethingChanged = false;
         for (let y = this.aggregateRiskMatrix.length - 1; y > -1 ; y--) {
             for (let x = this.aggregateRiskMatrix[y].length - 1; x > -1 ; x--) {
                 // check if UP is better:
@@ -78,15 +158,16 @@ module.exports = class SubmarineChitonAvoidanceAnalyzer {
                     // OK, up is better... Now what? ... Check if others depended on me, if so, update it and anyone who depended on him.
                     // console.log(`need to adjust X: ${x}, Y: ${y}.  ${this.aggregateRiskMatrix[y-1][x]} is less than ${this.aggregateRiskMatrix[y][x]}`);
                     this.cascadeAdjustments(this.aggregateRiskMatrix[y-1][x] + this.chitonRiskMatrix[y][x], x, y);
+                    somethingChanged = true;
                 }
                 if (x > 0 && (this.aggregateRiskMatrix[y][x-1] + this.chitonRiskMatrix[y][x]) < this.aggregateRiskMatrix[y][x]) {
                     // OK, left is better... Now what? ... Check if others depended on me, if so, update it and anyone who depended on him.
                     this.cascadeAdjustments(this.aggregateRiskMatrix[y][x-1] + this.chitonRiskMatrix[y][x], x, y);
+                    somethingChanged = true;
                 }
-
-
             }
         }
+        return somethingChanged;
     }
 
     cascadeAdjustments(newValue, x, y) {
