2e0066b8f5d484d409e14857d2ee271887959712
Dave <dmruczek@gmail.com>
2021-12-21 19:18:43 -0500

    completed day 17 part 2

--- day-17/part-2.js
+++ day-17/part-2.js
@@ -3,5 +3,4 @@ const probeLauncherTrajectoryAnalyzer = new ProbeLauncherTrajectoryAnalyzer();
 probeLauncherTrajectoryAnalyzer.loadInput('input.txt');
 
 console.log(probeLauncherTrajectoryAnalyzer.findAllSuccessfullFiringSolutions().length);
-// first try gave 1788, which is too low.
-// then got 1795, which is also too low.
+// answer was 1908
